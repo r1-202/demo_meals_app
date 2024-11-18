2483ee653eaddb6826d4cf86a324d0b897362ef3
Kassem Hmady <khmkhm626@gmail.com>
2024-11-18 03:28:09 +0200

    Added methods getAllMealsHTTP and saveMeals which handle requesting data and saving the data to a local database respectively.

---
+++ lib/utils/http_getters.dart
@@ -0,0 +1,37 @@
+library http_getters;
+
+import 'dart:convert';
+
+import '../models/meal.dart';
+import 'package:http/http.dart' as http;
+
+import '../services/database_services.dart';
+
+Future<List<Meal>> getAllMealsHTTP() async {
+  List<Meal> list = List<Meal>.empty(growable: true);
+  final alphabet = "abcdefghijklmnopqrstuvwxyz"
+      .runes
+      .map((int rune) => String.fromCharCode(rune));
+  for (var c in alphabet) {
+    http.Response response = await http
+        .get(Uri.parse("www.themealdb.com/api/json/v1/1/search.php?f=$c"));
+    if (response.statusCode != 200) {
+      continue;
+    }
+    Map<String, dynamic> jsonMap =
+        jsonDecode(response.body) as Map<String, dynamic>;
+    var listJson = jsonMap["meals"] as List<dynamic>;
+    for (var l in listJson) {
+      list.add(Meal.fromJson(l));
+    }
+  }
+  return list;
+}
+
+Future<void> saveMeals() async {
+  var list = await getAllMealsHTTP();
+  final db = DatabaseServices();
+  for (var meal in list) {
+    db.insertMeal(meal);
+  }
+}
